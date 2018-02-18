5d86fda2333f551df03bbb61f32ce51aaa817ea7
xkamisama <836124520@qq.com>
2018-02-18 17:21:33 +0800

    test

---
+++ src/main/java/com/xkami/lightnovel/LightnovelApplication.java
@@ -0,0 +1,12 @@
+package com.xkami.lightnovel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LightnovelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LightnovelApplication.class, args);
+	}
+}
